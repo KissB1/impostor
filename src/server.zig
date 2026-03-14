58748e6205d8afdfb78ebf993f43220ebe4ce472
Benedek Kiss <kiss.benedek.zoltan.x@gmail.com>
2026-03-14 12:20:53 +0100

    Output moved to another file, ./output/output.zig

--- src/server.zig
+++ src/server.zig
@@ -9,6 +9,7 @@ const xkb = @import("xkbcommon");
 const gpa = std.heap.c_allocator;
 
 pub const Keyboard = @import("./input/keyboard.zig").Keyboard;
+pub const Output = @import("./output/output.zig").Output;
 
 pub const Server = struct {
     wl_server: *wl.Server,
@@ -573,62 +574,6 @@ pub const Server = struct {
         var child = std.process.Child.init(&[_][]const u8{ "/bin/sh", "-c", cmd }, gpa);
         child.env_map = &env_map;
         try child.spawn();
-    }
-};
-
-const Output = struct {
-    server: *Server,
-    wlr_output: *wlr.Output,
-
-    frame: wl.Listener(*wlr.Output) = .init(handleFrame),
-    request_state: wl.Listener(*wlr.Output.event.RequestState) = .init(handleRequestState),
-    destroy: wl.Listener(*wlr.Output) = .init(handleDestroy),
-
-    // The wlr.Output should be destroyed by the caller on failure to trigger cleanup.
-    fn create(server: *Server, wlr_output: *wlr.Output) !void {
-        const output = try gpa.create(Output);
-
-        output.* = .{
-            .server = server,
-            .wlr_output = wlr_output,
-        };
-        wlr_output.events.frame.add(&output.frame);
-        wlr_output.events.request_state.add(&output.request_state);
-        wlr_output.events.destroy.add(&output.destroy);
-
-        const layout_output = try server.output_layout.addAuto(wlr_output);
-
-        const scene_output = try server.scene.createSceneOutput(wlr_output);
-        server.scene_output_layout.addOutput(layout_output, scene_output);
-    }
-
-    fn handleFrame(listener: *wl.Listener(*wlr.Output), _: *wlr.Output) void {
-        const output: *Output = @fieldParentPtr("frame", listener);
-
-        const scene_output = output.server.scene.getSceneOutput(output.wlr_output).?;
-        _ = scene_output.commit(null);
-
-        var now = posix.clock_gettime(posix.CLOCK.MONOTONIC) catch @panic("CLOCK_MONOTONIC not supported");
-        scene_output.sendFrameDone(&now);
-    }
-
-    fn handleRequestState(
-        listener: *wl.Listener(*wlr.Output.event.RequestState),
-        event: *wlr.Output.event.RequestState,
-    ) void {
-        const output: *Output = @fieldParentPtr("request_state", listener);
-
-        _ = output.wlr_output.commitState(event.state);
-    }
-
-    fn handleDestroy(listener: *wl.Listener(*wlr.Output), _: *wlr.Output) void {
-        const output: *Output = @fieldParentPtr("destroy", listener);
-
-        output.frame.link.remove();
-        output.request_state.link.remove();
-        output.destroy.link.remove();
-
-        gpa.destroy(output);
     }
 };
 
